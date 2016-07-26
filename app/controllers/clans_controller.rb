d7617046f58e8a5d2dc0568f35f4be68e048c082
Ollie Haydon-Mulligan <ollie.hm@cantab.net>
2016-07-26 08:32:45 +0000

    Extract clansdisplay service from clans controller

--- app/controllers/clans_controller.rb
+++ app/controllers/clans_controller.rb
@@ -1,30 +1,21 @@
 class ClansController < ApplicationController
     
   def index
-    @clan = Clan.new
-    set_display_clans
+    @clansdisplay = ClansDisplay.new(current_user)
   end
   
   def create
-    @clan = Clan.build_with(user: current_user, params: clan_params)
-    if @clan.save
-      redirect_to request.referer
-    else
-      flash.now[:errors] = @clan.errors.full_messages
-      set_display_clans
-      render :index
+    clan = Clan.build_with(user: current_user, params: clan_params)
+    unless clan.save
+      flash[:errors] = clan.errors.full_messages
     end
+    redirect_to request.referer
   end
   
   private
   
   def clan_params
     params.require(:clan).permit(:name,:description)
-  end
-  
-  def set_display_clans
-    @myclans = current_user.clans
-    @otherclans = Clan.all_except(@myclans)
   end
   
 end
---
+++ app/services/clans_display.rb
@@ -0,0 +1,21 @@
+class ClansDisplay
+  
+  attr_reader :user
+  
+  def initialize(user)
+    @user = user
+  end
+  
+  def new_clan
+    Clan.new
+  end
+  
+  def my_clans
+    @my_clans ||= user.clans
+  end
+  
+  def other_clans
+    Clan.all_except(my_clans)
+  end
+  
+end
