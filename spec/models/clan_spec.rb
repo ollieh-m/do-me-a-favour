d8e232a9cceed384a7c365c3017f70b98d7a8138
Ollie Haydon-Mulligan <ollie.hm@cantab.net>
2016-07-26 10:58:37 +0000

    Update clan tests

--- spec/models/clan_spec.rb
+++ spec/models/clan_spec.rb
@@ -10,19 +10,11 @@ describe Clan do
   context '#build_with' do
     it 'builds a new user_clan_relationship associated with a new clan and the specified user' do
       association_collection = spy(:association_collection)
-      clan = double(:clan, user_clan_relationships: association_collection)
+      clan = double(:clan, users: association_collection)
       allow(Clan).to receive(:new).and_return(clan)
       
-      Clan.build_with(user: :user, params: :params)
-      expect(association_collection).to have_received(:build).with({user: :user})
-    end
-  end
-  
-  context '#all_except' do
-    it 'returns all clans except those specified' do
-      allow(Clan).to receive(:all).and_return([:clan1,:clan2,:clan3])
-      result = Clan.all_except([:clan1,:clan2])
-      expect(result).to eq [:clan3]
+      Clan.build_with(:user, :params)
+      expect(association_collection).to have_received(:<<).with(:user)
     end
   end
     
