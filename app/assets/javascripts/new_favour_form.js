17cc29d6f5d62212d13d09f7ce8e932a2ee96795
Ollie Haydon-Mulligan <ollie.hm@cantab.net>
2016-08-02 14:24:38 +0000

    Add JQuery search box on favour form for selecting users to benefit from favour

--- app/assets/javascripts/new_favour_form.js
+++ app/assets/javascripts/new_favour_form.js
@@ -1,12 +1,30 @@
 $(document).ready(function(){
+  
   reveal('second');
   reveal('third');
   reveal('fourth');
+  
+  linkSearchToSelectInput('users-benefiting-search');
+
 });
 
 var reveal = function(step){
   $('#to_' + step + '_step').on('click', function(){
     $('#help_' + step + '_step').show();
-    $(this).hide()
-  })
-}
+    $(this).hide();
+  });
+};
+
+var linkSearchToSelectInput = function(searchBox){
+  $('#' + searchBox).on('input', function(){
+    var search = $(this).val();
+    $('select option').each(function(index){
+      var option = $(this);
+      if (option.text().match(new RegExp(search))) {
+        option.prop('selected',true);
+      } else {
+        option.prop('selected',false)
+      };
+    });
+  });
+};
